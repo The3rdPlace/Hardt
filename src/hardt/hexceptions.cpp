dee14002f9bc7d7ce575cd5d71293003d26d7788
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-04-25 23:26:19 +0200

    Move exception definition to file that can be included from all source files

---
+++ src/hardt/hexceptions.cpp
@@ -0,0 +1,31 @@
+#include "hexceptions.h"
+
+HException::HException(const char* reason):
+    _why(reason)
+{
+}
+
+const char* HException::what() const throw()
+{
+    return _why;
+}
+
+HInitializationException::HInitializationException(const char* reason):
+    HException(reason)
+{}
+
+HNetworkException::HNetworkException(const char* reason):
+    HException(reason)
+{}
+
+HFileIOException::HFileIOException(const char* reason):
+    HException(reason)
+{}
+
+HAudioIOException::HAudioIOException(const char* reason):
+    HException(reason)
+{}
+
+HNotImplementedException::HNotImplementedException(const char* reason):
+    HException(reason)
+{}
---
+++ src/hardt/include/hexceptions.h
@@ -0,0 +1,85 @@
+#ifndef __HEXCEPTIONS_H
+#define __HEXCEPTIONS_H
+
+#include <stdlib.h>
+#include <exception>
+
+class HException : public std::exception
+{
+    protected:
+
+        const char* _why;
+
+    public:
+
+        HException(const char* reason);
+
+        const char* what() const throw();
+};
+
+/*
+    Thrown by constructors if the object could not be initialized
+
+    Constructor:
+        HInitializationException(reason)
+*/
+class HInitializationException : public HException
+{
+    public:
+
+        HInitializationException(const char* reason);
+};
+
+/*
+    Thrown if a network connection fails
+
+    Constructor:
+        HNetworkException(reason)
+*/
+class HNetworkException : public HException
+{
+    public:
+
+        HNetworkException(const char* reason);
+};
+
+/*
+    Thrown if there is errors during file i/o
+
+    Constructor:
+        HFileIOException(reason)
+*/
+class HFileIOException : public HException
+{
+    public:
+
+        HFileIOException(const char* reason);
+};
+
+/*
+    Thrown if there is errors during audio i/o
+
+    Constructor:
+        HAudioIOException(reason)
+*/
+class HAudioIOException : public HException
+{
+    public:
+
+        HAudioIOException(const char* reason);
+};
+
+/*
+    Thrown by functions if they have not been implemneted
+
+    Constructor:
+        HNotImplementedException()
+*/
+class HNotImplementedException : public HException
+{
+    public:
+
+        HNotImplementedException(const char* reason = "Not implemented");
+};
+
+#endif
