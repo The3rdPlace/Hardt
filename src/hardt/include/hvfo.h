8893b883c34531b1a7542f69fccd32238e251d5b
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-05-15 23:36:55 +0200

    Use correct type for amplitude

--- src/hardt/include/hvfo.h
+++ src/hardt/include/hvfo.h
@@ -7,13 +7,12 @@ class HVfo : public HGenerator<T>
     private:
 
         H_SAMPLE_RATE _rate;
-        int _amplitude;
+        T _amplitude;
 
     public:
 
         HVfo(H_SAMPLE_RATE rate, int frequency, T amplitude, float phase = 0);
 
-        //int Read(T* dest, size_t blocksize);
         void SetFrequency(int frequency, float phase = 0);
 };
 
