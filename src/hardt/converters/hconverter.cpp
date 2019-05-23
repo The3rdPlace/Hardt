9bcd8c52cf88e22ccc93e1a07fa0581787abdc2f
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-05-24 00:18:57 +0200

    Added ready callback
    and also added a few log statements

--- src/hardt/converters/hconverter.cpp
+++ src/hardt/converters/hconverter.cpp
@@ -4,6 +4,18 @@
 #include "hconverter.h"
 
 template <class T>
+HConverter<T>::HConverter(void (*readyCallback)())
+{
+    HLog("HConverter()");
+}
+
+template <class T>
+void HConverter<T>::Ready()
+{
+    _readyCallback();
+}
+
+template <class T>
 int HConverter<T>::Write(T* src, size_t blocksize)
 {
     return Convert(src, blocksize);
@@ -12,6 +24,19 @@ int HConverter<T>::Write(T* src, size_t blocksize)
 /********************************************************************
 Explicit instantiation
 ********************************************************************/
+
+// HConverter
+template
+HConverter<int8_t>::HConverter(void (*readyCallback)());
+
+template
+HConverter<uint8_t>::HConverter(void (*readyCallback)());
+
+template
+HConverter<int16_t>::HConverter(void (*readyCallback)());
+
+template
+HConverter<int32_t>::HConverter(void (*readyCallback)());
 
 // Write()
 template
@@ -25,5 +50,18 @@ int HConverter<int16_t>::Write(int16_t* src, size_t blocksize);
 
 template
 int HConverter<int32_t>::Write(int32_t* src, size_t blocksize);
+
+// Ready()
+template
+void HConverter<int8_t>::Ready();
+
+template
+void HConverter<uint8_t>::Ready();
+
+template
+void HConverter<int16_t>::Ready();
+
+template
+void HConverter<int32_t>::Ready();
 
 #endif
--- src/hardt/include/hconverter.h
+++ src/hardt/include/hconverter.h
@@ -4,9 +4,21 @@
 template <class T>
 class HConverter : public HWriter<T>
 {
-    int Write(T* src, size_t blocksize);
+    private:
 
-    virtual int Convert(T* src, size_t blocksize) = 0;
+        void (*_readyCallback)();
+
+    protected:
+
+        void Ready();
+
+    public:
+
+        HConverter(void (*readyCallback)());
+
+        int Write(T* src, size_t blocksize);
+
+        virtual int Convert(T* src, size_t blocksize) = 0;
 };
 
 #endif
