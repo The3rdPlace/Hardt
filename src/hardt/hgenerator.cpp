146157cceed4627c66ed57f42e0e75ec2e8a551b
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-06-01 10:24:24 +0200

    Improve the generator slightly

--- src/hardt/hgenerator.cpp
+++ src/hardt/hgenerator.cpp
@@ -46,15 +46,10 @@ void HGenerator<T>::Calculate(H_SAMPLE_RATE rate, int frequency, T amplitude, fl
     // How many distinct samples can we have per cycle
     _samplesPerCycle = rate / frequency;
 
-    // Unless we the generator frequency coalesce exact with the sample frequency
-    // (samplerate being a harmonic of the generator frequency), 1 cycle is not
-    // enough as the sample points moves around the sinus curve.
-    // A simple solution - which might need more tweaking - is to calculate a bigger
-    // number of cycles. Experimentation seems to indicate that 100 seems to be the
-    // optimum factor, but still - there's plenty of places where we introduce small
-    // shifts and aliasing by using integer values (which is what the real world
-    // provides when working with basic soundcards)
-    _samplesPerCycle *= 100;
+    // If the frequency is very low, and we get too few samples per cycle,
+    // precision drops because the slight shifts back and forth due to rounding errors
+    // becomes noticable. If we drop below 50 samples per cycle, increase that number
+    _samplesPerCycle *= ( _samplesPerCycle < 50 ? 100 : 10);
 
     // Create lookup table
     HLog("Creating lookup table of size %d (%d bytes)", _samplesPerCycle, _samplesPerCycle * sizeof(T));
@@ -64,7 +59,6 @@ void HGenerator<T>::Calculate(H_SAMPLE_RATE rate, int frequency, T amplitude, fl
     for( int i = 0; i < _samplesPerCycle; i++ )
     {
         _lot[i] = amplitude * sin((((2 * M_PI * frequency) / rate) * i) + phase);
-        //std::cout << "lot[" << i << "] = " << _lot[i] << std::endl;
     }
 
     // Initial sample position
