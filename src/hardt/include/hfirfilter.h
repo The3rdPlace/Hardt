e9fb9bad3703c8db5854b566600120737302ca08
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-07-10 23:52:30 +0200

    Add method for retrieving the filter parameters

--- src/hardt/include/hfirfilter.h
+++ src/hardt/include/hfirfilter.h
@@ -59,6 +59,16 @@ class HFirFilter : public HFilter<T>
 
         void Filter(T* src, T* dest, size_t blocksize);
 
+        std::vector<float> GetCoefficients()
+        {
+            std::vector<float> coefficients;
+            for( int i = 0; i < _length; i++ )
+            {
+                coefficients.push_back(_coefficients[i]);
+            }
+            return coefficients;
+        }
+
         static HFirFilter<T>* Create(HWriter<T>* writer, size_t blocksize, char* coeffsFilename)
         {
             std::vector<float> coeffs = ReadCoeffsFromFile(coeffsFilename);
--- src/hardt/include/hiirfilter.h
+++ src/hardt/include/hiirfilter.h
@@ -60,6 +60,20 @@ class HIirFilter : public HFilter<T>
 
         void Filter(T* src, T* dest, size_t blocksize);
 
+        std::vector<float> GetCoefficients()
+        {
+            std::vector<float> coefficients;
+            for( int i = 0; i < _length; i++ )
+            {
+                coefficients.push_back(_aCoefficients[i]);
+            }
+            for( int i = 0; i <= _length; i++ )
+            {
+                coefficients.push_back(_bCoefficients[i]);
+            }
+            return coefficients;
+        }
+
         static HIirFilter<T>* Create(HWriter<T>* writer, size_t blocksize, char* coeffsFilename)
         {
             std::vector<float> coeffs = ReadCoeffsFromFile(coeffsFilename);
