6276a27b69756b84c893675cd5d15f5c80d190f2
Henrik Witt-Hansen <hwha@dbc.dk>
2019-07-22 23:01:28 +0200

    Add test for signallevel class

---
+++ src/test/hsignallevel_test.cpp
@@ -0,0 +1,322 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "test.h"
+
+class HSignalLevel_Test: public Test
+{
+    public:
+
+        void run()
+        {
+            UNITTEST(test_signallevel_int8);
+            UNITTEST(test_signallevel_uint8);
+            UNITTEST(test_signallevel_int16);
+            UNITTEST(test_signallevel_int32);
+        }
+
+        const char* name()
+        {
+            return "HSignalLevel";
+        }
+
+    private:
+
+        int Db;
+        int S;
+        int callback(HSignalLevelResult* result, size_t length)
+        {
+            std::cout << "DB " << result->Db << ", S " << result->S << std::endl;
+
+            Db = result->Db;
+            S = result->S;
+
+            return length;
+        }
+
+        void test_signallevel_int8()
+        {
+            // Signal, max amplitude = (+-) 100
+            HSineGenerator<int8_t> g(48000, 1000, 0);
+            int8_t input[1024];
+            g.Read(input, 1024);
+
+            auto wr = HCustomWriter<HSignalLevelResult>::Create<HSignalLevel_Test>(this, &HSignalLevel_Test::callback);
+            HSignalLevel<int8_t> siglevel(wr, 0);
+
+            // The low dynamic range for 8 bit samples reduces the possible signal level values
+            g.Calculate(1000, 0, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -54);
+            ASSERT_IS_EQUAL(S, 0);
+
+            g.Calculate(1000, 1, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -42);
+            ASSERT_IS_EQUAL(S, 2);
+
+            g.Calculate(1000, 2, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -36);
+            ASSERT_IS_EQUAL(S, 3);
+
+            g.Calculate(1000, 4, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -30);
+            ASSERT_IS_EQUAL(S, 4);
+
+            g.Calculate(1000, 8, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -24);
+            ASSERT_IS_EQUAL(S, 5);
+
+            g.Calculate(1000, 16, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -18);
+            ASSERT_IS_EQUAL(S, 6);
+
+            g.Calculate(1000, 32, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -12);
+            ASSERT_IS_EQUAL(S, 7);
+
+            g.Calculate(1000, 64, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -6);
+            ASSERT_IS_EQUAL(S, 8);
+
+            g.Calculate(1000, 127, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, 0);
+            ASSERT_IS_EQUAL(S, 9);
+
+            delete wr;
+        }
+
+        void test_signallevel_uint8()
+        {
+            ASSERT_IGNORE("Generators does not handle uint8 correctly - enable this test when fixed");
+
+            // Signal, max amplitude = (+-) 100
+            HSineGenerator<uint8_t> g(48000, 1000, 0);
+            uint8_t input[1024];
+            g.Read(input, 1024);
+
+            auto wr = HCustomWriter<HSignalLevelResult>::Create<HSignalLevel_Test>(this, &HSignalLevel_Test::callback);
+            HSignalLevel<uint8_t> siglevel(wr, 0);
+
+            // The low dynamic range for 8 bit samples reduces the possible signal level values
+            g.Calculate(1000, 0, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -54);
+            ASSERT_IS_EQUAL(S, 0);
+
+            g.Calculate(1000, 1, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -42);
+            ASSERT_IS_EQUAL(S, 2);
+
+            g.Calculate(1000, 2, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -36);
+            ASSERT_IS_EQUAL(S, 3);
+
+            g.Calculate(1000, 4, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -30);
+            ASSERT_IS_EQUAL(S, 4);
+
+            g.Calculate(1000, 8, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -24);
+            ASSERT_IS_EQUAL(S, 5);
+
+            g.Calculate(1000, 16, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -18);
+            ASSERT_IS_EQUAL(S, 6);
+
+            g.Calculate(1000, 32, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -12);
+            ASSERT_IS_EQUAL(S, 7);
+
+            g.Calculate(1000, 64, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -6);
+            ASSERT_IS_EQUAL(S, 8);
+
+            g.Calculate(1000, 127, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, 0);
+            ASSERT_IS_EQUAL(S, 9);
+
+            delete wr;
+        }
+
+        void test_signallevel_int16()
+        {
+            // Signal, max amplitude = (+-) 100
+            HSineGenerator<int16_t> g(48000, 1000, 0);
+            int16_t input[1024];
+            g.Read(input, 1024);
+
+            auto wr = HCustomWriter<HSignalLevelResult>::Create<HSignalLevel_Test>(this, &HSignalLevel_Test::callback);
+            HSignalLevel<int16_t> siglevel(wr, 0);
+
+            g.Calculate(1000, 0, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -54);
+            ASSERT_IS_EQUAL(S, 0);
+
+            g.Calculate(1000, 128, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -48);
+            ASSERT_IS_EQUAL(S, 1);
+
+            g.Calculate(1000, 256, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -42);
+            ASSERT_IS_EQUAL(S, 2);
+
+            g.Calculate(1000, 512, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -36);
+            ASSERT_IS_EQUAL(S, 3);
+
+            g.Calculate(1000, 1024, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -30);
+            ASSERT_IS_EQUAL(S, 4);
+
+            g.Calculate(1000, 2048, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -24);
+            ASSERT_IS_EQUAL(S, 5);
+
+            g.Calculate(1000, 4096, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -18);
+            ASSERT_IS_EQUAL(S, 6);
+
+            g.Calculate(1000, 8192, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -12);
+            ASSERT_IS_EQUAL(S, 7);
+
+            g.Calculate(1000, 16384, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -6);
+            ASSERT_IS_EQUAL(S, 8);
+
+            g.Calculate(1000, 32768, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, 0);
+            ASSERT_IS_EQUAL(S, 9);
+
+            delete wr;
+        }
+
+        void test_signallevel_int32()
+        {
+            // Signal, max amplitude = (+-) 100
+            HSineGenerator<int32_t> g(48000, 1000, 0);
+            int32_t input[1024];
+            g.Read(input, 1024);
+
+            auto wr = HCustomWriter<HSignalLevelResult>::Create<HSignalLevel_Test>(this, &HSignalLevel_Test::callback);
+            HSignalLevel<int32_t> siglevel(wr, 0);
+
+            // The high dynamic range for 32 bit samples will allow many lower db values, but all will register with S = 0
+            g.Calculate(1000, 0, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -54);
+            ASSERT_IS_EQUAL(S, 0);
+
+            g.Calculate(1000, 8388608, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -48);
+            ASSERT_IS_EQUAL(S, 1);
+
+            g.Calculate(1000, 16777216, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -42);
+            ASSERT_IS_EQUAL(S, 2);
+
+            g.Calculate(1000, 33554432, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -36);
+            ASSERT_IS_EQUAL(S, 3);
+
+            g.Calculate(1000, 67108864, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -30);
+            ASSERT_IS_EQUAL(S, 4);
+
+            g.Calculate(1000, 134217728, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -24);
+            ASSERT_IS_EQUAL(S, 5);
+
+            g.Calculate(1000, 268435456, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -18);
+            ASSERT_IS_EQUAL(S, 6);
+
+            g.Calculate(1000, 536870912, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -12);
+            ASSERT_IS_EQUAL(S, 7);
+
+            g.Calculate(1000, 1073741824, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, -6);
+            ASSERT_IS_EQUAL(S, 8);
+
+            g.Calculate(1000, 2147483648, 0);
+            g.Read(input, 1024);
+            siglevel.Write(input, 1024);
+            ASSERT_IS_EQUAL(Db, 0);
+            ASSERT_IS_EQUAL(S, 9);
+
+            delete wr;
+        }
+
+} hSignallevel_test;
