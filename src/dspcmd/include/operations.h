a034b61132fe0098f52cb953c6e0ebd07fe2f47e
Henrik Witt-Hansen <hwha@dbc.dk>
2019-08-13 10:14:00 +0200

    Rearrange sourcetree to fit debuild

--- src/dspcmd/include/operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OPERATIONS_H
-#define __OPERATIONS_H
-
-#include "config.h"
-
-int RunOperation();
-
-#endif
-
--- src/dspcmd/include/signalhandling.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SIGNALHANDLING_H
-#define __SIGNALHANDLING_H
-
-extern bool terminated;
-
-extern void SetupSignalHandling();
-
-#endif
