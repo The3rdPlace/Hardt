c979d567a63c6be40082f755e82e0d6c00dba0dc
Henrik Witt-Hansen <hwha@dbc.dk>
2019-08-25 22:30:45 +0200

    Add skeleton files for missing examples

---
+++ hardt/examples/readers_and_writers/readers_and_writers.cpp
@@ -0,0 +1,37 @@
+/*
+	Todo: add example
+*/
+
+#include <iostream>
+
+#include "hardtapi.h"
+
+int main(int argc, char** argv)
+{
+    // Initialize the Hardt toolkit.
+    // Set the last argument to 'true' to enable verbose output instead of logging to a local file
+    HInit(std::string("readers_and_writers"), true);
+
+    // Show application name and and Hardt version.
+    // This is not needed, just to have something on the screen
+    std::cout << "readers_and_writers: using Hardt " + getversion() << std::endl;
+
+    // Check that we got the required input parameters
+    if( argc < 3 )
+    {
+        std::cout << "Usage: readers_and_writers 'output-sound-device-number'" << std::endl;
+        std::cout << "Use 'dspcmd -a' to get a list of your sound device numbers" << std::endl;
+    }
+
+    // Decide on a block size to use - how many samples to pass into the chain on every
+    // read or write. This mainly affects memory consumption but performance may be
+    // reduced by large blocksizes since more data has to be moved between buffers
+    const int BLOCKSIZE = 4096;
+
+    // Todo: add more code
+}
+
+/**
+    \example readers_and_writers.cpp
+    How to link HReaders and HWriters in a chain
+ */
