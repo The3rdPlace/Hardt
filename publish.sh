3c34066a43d0baa77cc485b34b27427e6cd2f064
Henrik Witt-Hansen <hwha@dbc.dk>
2019-10-24 22:07:43 +0200

    Add helper script for publishing the distribution files

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get credentials
+if [ -f .publish ]; then
+  . .publish
+else
+  echo "Please create the file .publish with this content:"
+  echo "PUBUSER=you"
+  echo "PUBPASS=pass"
+  echo "PUBHOST=ftp.where.com"
+  echo "PUBROOT=put/here"
+  exit 1
+fi
+
+MAJOR=$(cat hardt/libhardt/include/hardt.h | grep MAJORVERSION | cut -d ' ' -f 3)
+MINOR=$(cat hardt/libhardt/include/hardt.h | grep MINORVERSION | cut -d ' ' -f 3)
+BUILD=$(cat hardt/libhardt/include/hardt.h | grep BUILDNO | cut -d ' ' -f 3)
+
+# Archives
+ncftpput -u $PUBUSER -p $PUBPASS $PUBHOST $PUBROOT/. ../dist/hardt_${MAJOR}.${MINOR}-${BUILD}_amd64.deb
+ncftpput -u $PUBUSER -p $PUBPASS $PUBHOST $PUBROOT/. ../dist/hardt_${MAJOR}.${MINOR}.orig.tar.gz
+
+# Documentation
+ncftpput -u $PUBUSER -p  $PUBPASS -R $PUBHOST $PUBROOT/. ../dist/hardt/docs/html/*
