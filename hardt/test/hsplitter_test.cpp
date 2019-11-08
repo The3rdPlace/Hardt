6c5c7e5d0c297b785d2f0c294a82f8253ab35363
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-11-08 23:58:36 +0100

    Add test that checks if the splitter ignores SetWriter() calls with an existing writer

--- hardt/test/hsplitter_test.cpp
+++ hardt/test/hsplitter_test.cpp
@@ -10,6 +10,7 @@ class HSplitter_Test: public Test
         {
             UNITTEST(test_write);
             UNITTEST(test_write_with_vector);
+            UNITTEST(test_ignore_multiple_setwriter_calls);
         }
 
         const char* name()
@@ -20,16 +21,18 @@ class HSplitter_Test: public Test
     private:
 
         template <class T>
-        class TestWriter : public HWriter<T>
+        class TestWriter : public HWriter<T>, public HWriterConsumer<T>
         {
             public:
 
                 T* Received;
+                int Writes;
 
                 TestWriter(size_t blocksize)
                 {
                     Received = new T[blocksize];
                     memset((void*) Received, 0, blocksize * sizeof(T));
+                    Writes = 0;
                 }
 
                 ~TestWriter()
@@ -40,8 +43,11 @@ class HSplitter_Test: public Test
                 int Write(T* src, size_t blocksize)
                 {
                     memcpy((void*) Received, (void*) src, blocksize * sizeof(T));
+                    Writes++;
                     return blocksize;
                 }
+
+                void SetWriter(HWriter<T>* writer) {}
         };
 
         void test_write()
@@ -84,6 +90,31 @@ class HSplitter_Test: public Test
             ASSERT_IS_EQUAL(memcmp((void*) writer1.Received, (void*) input, 10 * sizeof(int8_t)), 0);
             ASSERT_IS_EQUAL(memcmp((void*) writer2.Received, (void*) input, 10 * sizeof(int8_t)), 0);
             ASSERT_IS_EQUAL(memcmp((void*) writer3.Received, (void*) input, 10 * sizeof(int8_t)), 0);
+        }
+
+        void test_ignore_multiple_setwriter_calls()
+        {
+            TestWriter<int8_t> Parent(10);
+            TestWriter<int8_t> writer1(10);
+            TestWriter<int8_t> writer2(10);
+
+            HSplitter<int8_t> splitter(Parent.Consumer());
+            int8_t input[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            splitter.SetWriter(&writer1);
+            splitter.SetWriter(&writer2);
+            splitter.SetWriter(&writer2);
+
+            ASSERT_IS_NOT_EQUAL(memcmp((void*) writer1.Received, (void*) input, 10 * sizeof(int8_t)), 0);
+            ASSERT_IS_NOT_EQUAL(memcmp((void*) writer2.Received, (void*) input, 10 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_EQUAL(splitter.Write(input, 10), 10);
+
+            ASSERT_IS_EQUAL(memcmp((void*) writer1.Received, (void*) input, 10 * sizeof(int8_t)), 0);
+            ASSERT_IS_EQUAL(memcmp((void*) writer2.Received, (void*) input, 10 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_EQUAL(writer1.Writes, 1);
+            ASSERT_IS_EQUAL(writer2.Writes, 1);
         }
 
 } hsplitter_test;
