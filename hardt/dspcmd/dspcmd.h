9a659c37c16d1a16422dde199d14bfe069b8f8fc
Henrik Witt-Hansen <hwha@dbc.dk>
2020-01-23 16:48:07 +0100

    Inital changes to add a help page for dspcmd usage

--- hardt/dspcmd/dspcmd.h
+++ hardt/dspcmd/dspcmd.h
@@ -1,3 +1,23 @@
+/**
+    \page dspcmd
+
+	\section introduction_section_dspcmd About dspcmd
+	todo
+
+
+	\section overview_section_dspcmd Overview
+	todo
+
+
+	\section options_section_dspcmd Options
+
+	\subpage fmgp Show spectrum plot in GnuPlot
+
+
+	\page fmgp
+	Syntax: dspcmd -fmgp -if file.pcm
+*/
+
 #ifndef __DSPCMD_H
 #define __DSPCMD_H
 
