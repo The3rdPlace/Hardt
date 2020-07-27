4fdaa44d3cc5145d6b87ed099ab97ced89f7cd5b
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2020-07-27 16:49:57 +0200

    Make filereader- and writer take all kinds of types
    Also fix a small bug in the filereader where the reader would always
    return one block too many

--- hardt/libhardt/include/hfilewriter.h
+++ hardt/libhardt/include/hfilewriter.h
@@ -18,30 +18,68 @@ class HFileWriter : public HWriter<T>
     public:
 
         /** Construct a new HFileWriter with the given filename */
-        HFileWriter(const char* filename);
+        HFileWriter(const char* filename):
+                _filename(std::string(filename))
+        {}
 
         /** Construct a new HFileWriter with the given filename */
-        HFileWriter(const std::string filename);
+        HFileWriter(const std::string filename):
+                _filename(filename)
+        {}
 
         /** Construct a new HFileWriter with the given filename and register
             with the given consumer */
-        HFileWriter(const char* filename, HWriterConsumer<T>* consumer);
+        HFileWriter(const char* filename, HWriterConsumer<T>* consumer):
+                _filename(std::string(filename))
+        {
+            consumer->SetWriter(this);
+        }
 
         /** Construct a new HFileWriter with the given filename and register
             with the given consumer */
-        HFileWriter(const std::string filename, HWriterConsumer<T>* consumer);
+        HFileWriter(const std::string filename, HWriterConsumer<T>* consumer):
+                _filename(filename)
+        {
+            consumer->SetWriter(this);
+        }
 
         /** Write to the file */
-        virtual int Write(T* src, size_t blocksize);
+        virtual int Write(T* src, size_t blocksize)
+        {
+            _stream.write((char*) src, blocksize * sizeof(T));
+            this->Metrics.Writes++;
+            this->Metrics.BlocksOut++;
+            this->Metrics.BytesOut += blocksize * sizeof(T);
+            return blocksize;
+        }
 
         /** Initialize before first write */
-        bool Start();
+        bool Start()
+        {
+            HLog("Trying to open stream for %s", _filename.c_str());
+            _stream.open(_filename.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+            if( !_stream.is_open())
+            {
+                HError("Failed to open file %s", _filename.c_str());
+                return false;
+            }
+            HLog("Stream is open");
+            return true;
+        }
 
         /** Cleanup after last write */
-        bool Stop();
+        bool Stop()
+        {
+            HLog("Closing stream");
+            _stream.close();
+            return true;
+        }
 
         /** Seek to this position in the file */
-        void Seek(int bytes);
+        void Seek(int bytes)
+        {
+            _stream.seekp(bytes, std::ios::beg);
+        }
 
         /** Execute or carry through a command */
         bool Command(HCommand* command) {
