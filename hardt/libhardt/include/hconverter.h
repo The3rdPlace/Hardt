47627f9357cdbbbff172e19f7457e319cfdedd16
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2020-07-29 14:28:34 +0200

    Make the converter handle few-to-may and many-to-few conversions

--- hardt/libhardt/include/hconverter.h
+++ hardt/libhardt/include/hconverter.h
@@ -14,7 +14,8 @@ class HConverter : public HReader<Tout>, public HWriter<Tin>, public HWriterCons
 
         HReader<Tin>* _reader;
         HWriter<Tout>* _writer;
-        size_t _blocksize;
+        size_t _blocksizeIn;
+        size_t _blocksizeOut;
 
         Tin *_input;
         Tout *_output;
@@ -36,31 +37,34 @@ class HConverter : public HReader<Tout>, public HWriter<Tin>, public HWriterCons
     protected:
 
         /** Construct a new HConverter that reads from a reader */
-        HConverter(HReader<Tin>* reader, size_t blocksize):
+        HConverter(HReader<Tin>* reader, size_t blocksizeIn, size_t blocksizeOut):
             _reader(reader),
             _writer(nullptr),
-            _blocksize(blocksize),
+            _blocksizeIn(blocksizeIn),
+            _blocksizeOut(blocksizeOut),
             _output(NULL)
         {
-            _input = new Tin[blocksize];
+            _input = new Tin[blocksizeIn];
         }
 
         /** Construct a new HConverter that writes to a writer */
-        HConverter(HWriter<Tout>* writer, size_t blocksize):
+        HConverter(HWriter<Tout>* writer, size_t blocksizeIn, size_t blocksizeOut):
             _reader(nullptr),
             _writer(writer),
-            _blocksize(blocksize),
+            _blocksizeIn(blocksizeIn),
+            _blocksizeOut(blocksizeOut),
             _input(NULL)
         {
-            _output = new Tout[blocksize];
+            _output = new Tout[blocksizeOut];
         }
 
         /** Construct a new HConverter that registers with an upstream writer */
-        HConverter(HWriterConsumer<Tin>* consumer, size_t blocksize):
-            _blocksize(blocksize),
+        HConverter(HWriterConsumer<Tin>* consumer, size_t blocksizeIn, size_t blocksizeOut):
+            _blocksizeIn(blocksizeIn),
+            _blocksizeOut(blocksizeOut),
             _input(NULL)
         {
-            _output = new Tout[blocksize];
+            _output = new Tout[blocksizeOut];
             consumer->SetWriter(this->Writer());
         }
 
@@ -81,57 +85,57 @@ class HConverter : public HReader<Tout>, public HWriter<Tin>, public HWriterCons
 
         /** Read a block of converted samples */
         int Read(Tout* dest, size_t blocksize) {
-            if( blocksize != _blocksize )
+            if( blocksize != _blocksizeOut )
             {
-                HError("Request for conversion-read with blocksize %d expected %d blocks", blocksize, _blocksize);
+                HError("Request for conversion-read with blocksize %d expected %d blocks", blocksize, _blocksizeOut);
                 throw new HConverterIOException("Invalid number of blocks");
             }
 
-            int read = _reader->Read(_input, blocksize);
+            int read = _reader->Read(_input, _blocksizeIn);
             if( read == 0 )
             {
                 HLog("Zero length read. Returning eof (zero)");
                 return 0;
             }
-            if( read != blocksize )
+            if( read != _blocksizeIn )
             {
-                HError("Request for read with blocksize %d returned %d blocks", blocksize, read);
+                HError("Request for read with blocksize %d returned %d blocks", _blocksizeIn, read);
                 throw new HConverterIOException("Invalid number of blocks");
             }
 
             int converted;
-            if( (converted = Convert(_input, dest, blocksize)) != blocksize )
+            if( (converted = Convert(_input, dest, _blocksizeIn)) != _blocksizeOut )
             {
-                HError("Request for conversion of blocksize %d blocks converted %d blocks", blocksize, converted);
+                HError("Request for conversion of blocksize %d blocks converted %d blocks", _blocksizeIn, converted);
                 throw new HConverterIOException("Converted incorrect number of blocks");
             }
 
-            return blocksize;
+            return _blocksizeOut;
         };
 
         /** Write a block of samples to convert */
         int Write(Tin* src, size_t blocksize) {
-            if( blocksize != _blocksize )
+            if( blocksize != _blocksizeIn )
             {
-                HError("Request for conversion-write with blocksize %d expected %d blocks", blocksize, _blocksize);
+                HError("Request for conversion-write with blocksize %d expected %d blocks", blocksize, _blocksizeIn);
                 throw new HConverterIOException("Read incorrect number of blocks from input reader");
             }
 
             int converted;
-            if( (converted = Convert(src, _output, blocksize)) != blocksize )
+            if( (converted = Convert(src, _output, _blocksizeIn)) != _blocksizeOut )
             {
-                HError("Request for conversion of blocksize %d blocks converted %d blocks", blocksize, converted);
+                HError("Request for conversion of blocksize %d blocks converted %d blocks", _blocksizeIn, converted);
                 throw new HConverterIOException("Converted incorrect number of blocks");
             }
 
             int written;
-            if( (written = _writer->Write(_output, blocksize)) != blocksize )
+            if( (written = _writer->Write(_output, _blocksizeOut)) != _blocksizeOut )
             {
-                HError("Request for write with blocksize %d wrote %d blocks", blocksize, written);
+                HError("Request for write with blocksize %d wrote %d blocks", _blocksizeOut, written);
                 throw new HConverterIOException("Wrote incorrect number of blocks to output writer");
             }
 
-            return blocksize;
+            return _blocksizeIn;
         }
 
         /** Convert a block of samples */
--- hardt/test/hconverter_test.cpp
+++ hardt/test/hconverter_test.cpp
@@ -15,6 +15,10 @@ class HConverter_Test: public Test
             UNITTEST(test_converter_int8_int8_write_overflow);
             UNITTEST(test_converter_int8_int16_read);
             UNITTEST(test_converter_int8_int16_write);
+            UNITTEST(test_converter_int8_int8_read_1_2);
+            UNITTEST(test_converter_int8_int8_write_1_2);
+            UNITTEST(test_converter_int8_int8_read_2_1);
+            UNITTEST(test_converter_int8_int8_write_2_1);
         }
 
         const char* name()
@@ -45,17 +49,75 @@ class HConverter_Test: public Test
             public:
 
                 TestConverter(HReader<Tin>* reader, size_t blocksize, int factor):
-                    HConverter<Tin, Tout>(reader, blocksize),
+                    HConverter<Tin, Tout>(reader, blocksize, blocksize),
                     _factor(factor)
                 {}
 
                 TestConverter(HWriter<Tout>* writer, size_t blocksize, int factor):
-                    HConverter<Tin, Tout>(writer, blocksize),
+                    HConverter<Tin, Tout>(writer, blocksize, blocksize),
                     _factor(factor)
                 {}
         };
 
-        void test_converter_int8_int8_read()
+        template <class Tin, class Tout>
+        class TestConverter_1_2 : public HConverter<Tin, Tout>
+        {
+            private:
+
+                int _factor;
+
+                int Convert(Tin* src, Tout* dest, size_t blocksize)
+                {
+                    for( int i = 0; i < blocksize * 2; i++ )
+                    {
+                        dest[i] = src[i / 2] * _factor;
+                    }
+                    return blocksize * 2;
+                }
+
+            public:
+
+                TestConverter_1_2(HReader<Tin>* reader, size_t blocksize, int factor):
+                        HConverter<Tin, Tout>(reader, blocksize, blocksize * 2),
+                        _factor(factor)
+                {}
+
+                TestConverter_1_2(HWriter<Tout>* writer, size_t blocksize, int factor):
+                        HConverter<Tin, Tout>(writer, blocksize, blocksize * 2),
+                        _factor(factor)
+                {}
+        };
+
+    template <class Tin, class Tout>
+    class TestConverter_2_1 : public HConverter<Tin, Tout>
+    {
+        private:
+
+            int _factor;
+
+            int Convert(Tin* src, Tout* dest, size_t blocksize)
+            {
+                for( int i = 0; i < blocksize; i++ )
+                {
+                    dest[i / 2] = src[i] * _factor;
+                }
+                return blocksize / 2;
+            }
+
+        public:
+
+            TestConverter_2_1(HReader<Tin>* reader, size_t blocksize, int factor):
+                    HConverter<Tin, Tout>(reader, blocksize, blocksize / 2),
+                    _factor(factor)
+            {}
+
+            TestConverter_2_1(HWriter<Tout>* writer, size_t blocksize, int factor):
+                    HConverter<Tin, Tout>(writer, blocksize, blocksize / 2),
+                    _factor(factor)
+            {}
+    };
+
+    void test_converter_int8_int8_read()
         {
             int8_t input[6] = {1, 2, 3, 4, 5, 6};
             int8_t expected[6] = {2, 4, 6, 8, 10, 12};
@@ -164,6 +226,92 @@ class HConverter_Test: public Test
             converter.Write(input, 6);
 
             for(int i = 0; i < 6; i++)
+            {
+                ASSERT_IS_EQUAL(writer.Received[i], expected[i]);
+            }
+
+            ASSERT_IS_TRUE(converter.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(writer.Commands, 1);
+        }
+
+        void test_converter_int8_int8_read_1_2()
+        {
+            int8_t input[6] = {1, 2, 3, 4, 5, 6};
+            int8_t expected[12] = {2, 2, 4, 4, 6, 6, 8, 8, 10, 10, 12, 12};
+            int8_t output[12] = {0, 0 ,0 ,0 ,0 ,0, 0, 0 ,0 ,0 ,0 ,0};
+
+            TestReader<int8_t> reader(input, 6);
+            TestConverter_1_2<int8_t, int8_t> converter(&reader, 6, 2);
+
+            ASSERT_IS_EQUAL(converter.Read(output, 12), 12);
+            ASSERT_IS_EQUAL(reader.Reads, 1);
+            ASSERT_IS_EQUAL(reader.Samples, 6);
+
+            for(int i = 0; i < 12; i++)
+            {
+                ASSERT_IS_EQUAL(output[i], expected[i]);
+            }
+
+            ASSERT_IS_TRUE(converter.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(reader.Commands, 1);
+        }
+
+        void test_converter_int8_int8_write_1_2()
+        {
+            int8_t input[6] = {1, 2, 3, 4, 5, 6};
+            int8_t expected[12] = {3, 3, 6, 6, 9, 9, 12, 12, 15, 15, 18, 18};
+
+            TestWriter<int8_t> writer(12);
+            TestConverter_1_2<int8_t, int8_t> converter(&writer, 6, 3);
+
+            ASSERT_IS_EQUAL(converter.Write(input, 6), 6);
+            ASSERT_IS_EQUAL(writer.Writes, 1);
+            ASSERT_IS_EQUAL(writer.Samples, 12);
+
+            for(int i = 0; i < 12; i++)
+            {
+                ASSERT_IS_EQUAL(writer.Received[i], expected[i]);
+            }
+
+            ASSERT_IS_TRUE(converter.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(writer.Commands, 1);
+        }
+
+        void test_converter_int8_int8_read_2_1()
+        {
+            int8_t input[12] = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6};
+            int8_t expected[6] = {4, 8, 12, 4, 8, 12};
+            int8_t output[6] = {0, 0 ,0 ,0 ,0 ,0};
+
+            TestReader<int8_t> reader(input, 6);
+            TestConverter_2_1<int8_t, int8_t> converter(&reader, 12, 2);
+
+            ASSERT_IS_EQUAL(converter.Read(output, 6), 6);
+            ASSERT_IS_EQUAL(reader.Reads, 1);
+            ASSERT_IS_EQUAL(reader.Samples, 12);
+
+            for(int i = 0; i < 6; i++)
+            {
+                ASSERT_IS_EQUAL(output[i], expected[i]);
+            }
+
+            ASSERT_IS_TRUE(converter.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(reader.Commands, 1);
+        }
+
+        void test_converter_int8_int8_write_2_1()
+        {
+            int8_t input[12] = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6};
+            int8_t expected[12] = {6, 12, 18, 6, 12, 18};
+
+            TestWriter<int8_t> writer(12);
+            TestConverter_2_1<int8_t, int8_t> converter(&writer, 12, 3);
+
+            ASSERT_IS_EQUAL(converter.Write(input, 12), 12);
+            ASSERT_IS_EQUAL(writer.Writes, 1);
+            ASSERT_IS_EQUAL(writer.Samples, 6);
+
+            for(int i = 0; i < 12; i++)
             {
                 ASSERT_IS_EQUAL(writer.Received[i], expected[i]);
             }
