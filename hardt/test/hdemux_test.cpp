9d9acc4b250f5af50b082e8e66f473ab33051ce4
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2020-09-10 15:58:06 +0200

    Add test for using hdemux with consumers

--- hardt/test/hdemux_test.cpp
+++ hardt/test/hdemux_test.cpp
@@ -11,6 +11,7 @@ class HDeMux_Test: public Test
         {
             UNITTEST(test_demux_write);
             UNITTEST(test_demux_read);
+            UNITTEST(test_demux_consume);
         }
 
         const char* name()
@@ -65,6 +66,29 @@ class HDeMux_Test: public Test
             ASSERT_IS_EQUAL(rd.Reads, 2);
             ASSERT_IS_EQUAL(rd.Samples, 32);
             ASSERT_IS_EQUAL(memcmp((void*) received, (void*) expected_2, 8 * sizeof(int8_t)), 0);
+        }
+
+        void test_demux_consume()
+        {
+            TestWriter<int8_t> srcWr(8);
+            HDeMux<int8_t> demux(srcWr.Consumer(), 8);
+            TestWriter<int8_t> wr_1(demux.Consumer(), 8);
+            TestWriter<int8_t> wr_2(demux.Consumer(), 8);
+            int8_t input[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+            int8_t expected_1[8] = {1, 3, 5, 7, 0, 0, 0, 0};
+            int8_t expected_2[8] = {2, 4, 6, 8, 0, 0, 0, 0};
+
+            ASSERT_IS_EQUAL(srcWr.Write(input, 8), 8);
+            ASSERT_IS_EQUAL(wr_1.Writes, 1);
+            ASSERT_IS_EQUAL(wr_1.Samples, 4);
+            ASSERT_IS_EQUAL(memcmp((void*) wr_1.Received, (void*) expected_1, 8 * sizeof(int8_t)), 0);
+            ASSERT_IS_EQUAL(wr_2.Writes, 1);
+            ASSERT_IS_EQUAL(wr_2.Samples, 4);
+            ASSERT_IS_EQUAL(memcmp((void*) wr_2.Received, (void*) expected_2, 8 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_TRUE(demux.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(wr_1.Commands, 1);
+            ASSERT_IS_EQUAL(wr_2.Commands, 1);
         }
 
 } hdemux_test;
