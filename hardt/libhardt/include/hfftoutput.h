a6c042f534f2ec2de477713315b0d05a4ed0573c
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2020-09-12 22:12:10 +0200

    Fixup documentation to match correct doxygen and InteliJ IDE standards

--- hardt/libhardt/include/hfftoutput.h
+++ hardt/libhardt/include/hfftoutput.h
@@ -16,13 +16,19 @@
 */
 struct HFftResults
 {
-    /** The calculated/measured spectrum */
+    /**
+     * The calculated/measured spectrum
+     */
     double* Spectrum;
 
-    /** The complex spectrum, in case you need to do phase calculations */
+    /**
+     * The complex spectrum, in case you need to do phase calculations
+     */
     std::complex<double>* Result;
 
-    /** Size of spectrum (bins) */
+    /**
+     * Size of spectrum (bins)
+     */
     int Size;
 };
 
@@ -66,63 +72,61 @@ class HFftOutput : public HOutput<T, HFftResults>
     public:
 
         /** Create a new HFft output that writes to a writer
-
-            Parameters:
-
-              size = The FFT size (1024 is a good choice for analytic use, 256 is recommended for realtime applications)
-
-              average = The number of FFT's to accumulate and average before calling the result writer. Note that if you
-                        use anything but 1, you can not run your signal through the output multiple times since that
-                        would mix results in the averaging phase (say if you where zooming in)
-
-              writer = The result writer (most likely a HCustomerWriter<HFftResults>*)
-
-              window = The window to use before calculating the FFT
-
-              zoomRate = The samplerate of the input samples
-
-              zoomFactor = Zoom factor. If larger than 1, then the spectrum is zoomed around 'center' (in relation to 'size').
-                           The factor must divide up into 'size' with an integer result (1024/4 = ok, 1024/3 = not-ok!).
-
-              zoomCenter = The center bin (frequency), must be in the [0-size/2] range. Use the static HFftOutput::BinAtFrequency()
-                           method if you do not already know the bin number.
-
-              If you intend to use scaling, you _must_ initialize the HFftOutput with a samplerate. You can set the factor to 1
-              initially to disable zooming. But if you have not provided zoomRate != 0, zooming will not be setup and be available.!
-
-        */
+         *
+         * @param size The FFT size (1024 is a good choice for analytic use,
+         *        256 is recommended for realtime applications)
+         * @param average The number of FFT's to accumulate and average before
+         *                calling the result writer. Note that if you use anything
+         *                but 1, you can not run your signal through the output
+         *                multiple times since that would mix results in the averaging
+         *                phase (say if you where zooming in)
+         * @param writer The result writer (most likely a HCustomerWriter<HFftResults>*)
+         * @param window The window to use before calculating the FFT
+         * @param zoomRate The samplerate of the input samples
+         * @param zoomFactor Zoom factor. If larger than 1, then the spectrum is zoomed
+         *                   around 'center' (in relation to 'size'). The factor must
+         *                   divide up into 'size' with an integer result (1024/4 = ok,
+         *                   1024/3 = not-ok!).
+         * @param zoomCenter The center bin (frequency), must be in the [0-size/2] range.
+         *                   Use the static HFftOutput::BinAtFrequency() method if you do
+         *                   not already know the bin number.
+         *
+         * If you intend to use scaling, you _must_ initialize the HFftOutput with a
+         * samplerate. You can set the factor to 1 initially to disable zooming. But if you
+         * have not provided zoomRate != 0, zooming will not be setup and be available.!
+         */
         HFftOutput(int size, int average, HWriter<HFftResults>* writer, HWindow<T>* window, H_SAMPLE_RATE zoomRate = 0, int zoomFactor = 1, int zoomCenter = 0);
 
-        /** Create a new HFft output that registers with an upstream writer
-
-            Parameters:
-
-              size = The FFT size (1024 is a good choice for analytic use, 256 is recommended for realtime applications)
-
-              average = The number of FFT's to accumulate and average before calling the result writer. Note that if you
-                        use anything but 1, you can not run your signal through the output multiple times since that
-                        would mix results in the averaging phase (say if you where zooming in)
-
-              consumer = The upstream writer that this HFftOutput registers with. Make sure that you create a HWriter<HFftResults>
-                         that registers with this HFftOutput before pushing samples through.  (in most cases you would use
-                         a HCustomWriter<HFftResults> object)
-
-              window = The window to use before calculating the FFT
-
-              zoomRate = The samplerate of the input samples
-
-              zoomFactor = Zoom factor. If larger than 1, then the spectrum is zoomed around 'center' (in relation to 'size').
-                           The factor must divide up into 'size' with an integer result (1024/4 = ok, 1024/3 = not-ok!).
-
-              zoomCenter = The center bin (frequency), must be in the [0-size/2] range. Use the static HFftOutput::BinAtFrequency()
-                           method if you do not already know the bin number.
-
-              If you intend to use scaling, you _must_ initialize the HFftOutput with a samplerate. You can set the factor to 1
-              initially to disable zooming. But if you have not provided zoomRate != 0, zooming will not be setup and be available.!
-        */
+        /**
+         * Create a new HFft output that registers with an upstream writer
+         *
+         * @param size The FFT size (1024 is a good choice for analytic use,
+         *        256 is recommended for realtime applications)
+         * @param average The number of FFT's to accumulate and average before
+         *                calling the result writer. Note that if you use anything
+         *                but 1, you can not run your signal through the output
+         *                multiple times since that would mix results in the averaging
+         *                phase (say if you where zooming in)
+         * @param consumer The upstream writer that should write to this component
+         * @param window The window to use before calculating the FFT
+         * @param zoomRate The samplerate of the input samples
+         * @param zoomFactor Zoom factor. If larger than 1, then the spectrum is zoomed
+         *                   around 'center' (in relation to 'size'). The factor must
+         *                   divide up into 'size' with an integer result (1024/4 = ok,
+         *                   1024/3 = not-ok!).
+         * @param zoomCenter The center bin (frequency), must be in the [0-size/2] range.
+         *                   Use the static HFftOutput::BinAtFrequency() method if you do
+         *                   not already know the bin number.
+         *
+         * If you intend to use scaling, you _must_ initialize the HFftOutput with a
+         * samplerate. You can set the factor to 1 initially to disable zooming. But if you
+         * have not provided zoomRate != 0, zooming will not be setup and be available.!
+         */
         HFftOutput(int size, int average, HWriterConsumer<T>* consumer, HWindow<T>* window, H_SAMPLE_RATE zoomRate = 0, int zoomFactor = 1, int zoomCenter = 0);
 
-        /** Default destructor */
+        /**
+         * Default destructor
+         */
         ~HFftOutput()
         {
             delete _spectrum;
@@ -138,16 +142,22 @@ class HFftOutput : public HOutput<T, HFftResults>
             }
         }
 
-        /** Process a block of samples */
+        /**
+         * Process a block of samples
+         */
         int Output(T* src, size_t size);
 
-        /** Execute or carry through a command */
+        /**
+         * Execute or carry through a command
+         */
         bool Command(HCommand* command) {
             // No Further propagation of commands
             return true;
         }
 
-        /** Get the width of the FFT spectrum (how many frequencies is covered) */
+        /**
+         * Get the width of the FFT spectrum (how many frequencies is covered)
+         */
         int Width() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report FFT frequency span");
@@ -157,7 +167,9 @@ class HFftOutput : public HOutput<T, HFftResults>
             return (_zoomRate / 2) / _zoomFactor;
         }
 
-        /** Get the leftmost frequency of the selected fft window */
+        /**
+         * Get the leftmost frequency of the selected fft window
+         */
         int Left() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report left FFT frequency");
@@ -167,7 +179,9 @@ class HFftOutput : public HOutput<T, HFftResults>
             return Center() - (Width() / 2);
         }
 
-        /** Get the rightmost frequency of the selected fft window */
+        /**
+         * Get the rightmost frequency of the selected fft window
+         */
         int Right() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report right FFT frequency");
@@ -177,7 +191,9 @@ class HFftOutput : public HOutput<T, HFftResults>
             return Center() + (Width() / 2);
         }
 
-        /** Get the center FFT frequency */
+        /**
+         * Get the center FFT frequency
+         */
         int Center() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report center FFT frequency");
@@ -189,7 +205,9 @@ class HFftOutput : public HOutput<T, HFftResults>
                            : _zoomCenter;
         }
 
-        /** Return the number of frequencies per bin */
+        /**
+         * Return the number of frequencies per bin
+         */
         float BinSize() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report FFT bin size");
@@ -199,12 +217,16 @@ class HFftOutput : public HOutput<T, HFftResults>
             return (((float) _zoomRate / 2) / ((float) _size / 2)) / (float) _zoomFactor;
         }
 
-        /** Given a samplerate, return the number of frequencies per bin */
+        /**
+         * Given a samplerate, return the number of frequencies per bin
+         */
         float BinSize(H_SAMPLE_RATE rate) {
             return ((float) rate / 2) / ((float) _size / 2);
         }
 
-        /** Return the output sample rate after resampling */
+        /**
+         * Return the output sample rate after resampling
+         */
         H_SAMPLE_RATE Rate() {
             if( !_zoomEnabled ) {
                 HError("HFftOutput with zoom disabled cannot report FFT rate");
@@ -214,7 +236,9 @@ class HFftOutput : public HOutput<T, HFftResults>
             return _zoomRate / _zoomFactor;
         }
 
-        /** Change zoom factor and center */
+        /**
+         * Change zoom factor and center
+         */
         void SetZoom(int zoomFactor, int zoomCenter);
 };
 
