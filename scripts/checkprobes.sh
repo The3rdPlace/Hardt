4dd04864072f0e58caaba549e7cfadac56ae532e
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2020-09-15 23:14:55 +0200

    Also check for probe usage if a probe parameter exists

--- scripts/checkprobes.sh
+++ scripts/checkprobes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IGNORED=("HBreaker HWindow HStreamProcessor HNetworkProcessor HTimer HFft HFir HOutput HNotchBiQuad HPeakingEQBiQuad HLowpassBiQuad HAllpassBiQuad HHighShelvingBiQuad HHighpassBiQuad HLowShelvingBiQuad HBandpassBiQuad HBiQuad HNullWriter HNullReader HSineGenerator HCosineGenerator HMute HMux HDeMux HLocalOscillator HKaiserBessel HLowpassKaiserBessel HHighpassKaiserBessel HBandpassKaiserBessel HBandstopKaiserBessel HGoertzelOutput HMemoryWriter HMetrics HObject HBufferedWriter HChunkWriter HCustomWriter HFade HFftOutput HFileWriter HGenerator HFileReader HInputWriter HMemoryReader HNetworkReader HNetworkWriter HProcessor HSignalLevelOutput HSoundcardWriter HSplitter HVfo HWav HWavWriter")
+IGNORED=("HBreaker HWindow HStreamProcessor HNetworkProcessor HTimer HFft HFir HOutput HNotchBiQuad HPeakingEQBiQuad HLowpassBiQuad HAllpassBiQuad HHighShelvingBiQuad HHighpassBiQuad HLowShelvingBiQuad HBandpassBiQuad HBiQuad HNullWriter HNullReader HSineGenerator HCosineGenerator HMute HMux HDeMux HLocalOscillator HKaiserBessel HLowpassKaiserBessel HHighpassKaiserBessel HBandpassKaiserBessel HBandstopKaiserBessel HGoertzelOutput HMemoryWriter HMetrics HObject HBufferedWriter HChunkWriter HCustomWriter HFade HFftOutput HFileWriter HGenerator HFileReader HInputWriter HMemoryReader HNetworkReader HNetworkWriter HProcessor HSignalLevelOutput HSoundcardWriter HSplitter HVfo HWav HWavWriter HComplex2MultiplexedConverter HComplexConverter HConverter HMultiplexed2ComplexConverter HProbe HSwitch HTypeConverter")
 
 for HEADER in $(ls ../hardt/libhardt/include); do
-  CLASSNAME=$(cat ../hardt/libhardt/include/$HEADER | grep 'class ' | grep -v 'template' | cut -d ' ' -f 2 | tr -d '\n')
+  CLASSNAME=$(cat ../hardt/libhardt/include/$HEADER | grep 'class ' | grep -v 'template' | cut -d ' ' -f 2 | tr -d '\n:')
 
   IGNORE=0
   for IGNOREDCLASS in $IGNORED; do
@@ -14,19 +14,68 @@ for HEADER in $(ls ../hardt/libhardt/include); do
 
   if [ $IGNORE -eq 0 ]; then
     MISSING=0
-    cat ../hardt/libhardt/include/$HEADER  | grep -E "^ +${CLASSNAME}\(" | grep -v -E ".+HProbe<.+>*.+" | while read -r CONSTRUCTOR; do
+    cat ../hardt/libhardt/include/${HEADER}  | grep -E "^ +${CLASSNAME}\(" | grep -v -E ".+HProbe<.+>*.+" | while read -r CONSTRUCTOR; do
       if [ $MISSING -eq 0 ]; then
         echo
-        echo "Header: $HEADER"
+        echo "Header: ../hardt/libhardt/include/$HEADER"
         echo "Classname: $CLASSNAME"
-        echo "Reason: Missing declaration"
+        echo "  Reason: Missing declaration"
         MISSING=1
-        export SUM=$((SUM+1))
       fi
       echo "  $CONSTRUCTOR"
     done
 
-   # Todo: check implementation
+    MISSING=0
+    cat ../hardt/libhardt/include/${HEADER}  | grep -E "^ +${CLASSNAME}\(" | grep -E ".+HProbe<.+>*.+" | while read -r CONSTRUCTOR; do
+      IMPLEMENTATION=$(find ../hardt/libhardt/ | grep $(echo $HEADER | sed 's/.h/.cpp/'))
+
+      cat $IMPLEMENTATION | grep 'probe->Write' &> /dev/null
+      if [ $? -ne 0 ]; then
+
+        cat ../hardt/libhardt/include/$HEADER | grep 'probe->Write' &> /dev/null
+        if [ $? -ne 0 ]; then
+
+          cat $IMPLEMENTATION | grep 'HFilter' | grep -E "^ +HFilter<T>(.+, probe).+" &> /dev/null
+          if [ $? -ne 0 ]; then
+
+
+            cat ../hardt/libhardt/include/$HEADER | grep 'HFilter' | grep -E "^ +HFilter<T>(.+, probe).+" &> /dev/null
+            if [ $? -ne 0 ]; then
+
+
+              cat $IMPLEMENTATION | grep 'HGain' | grep -E "^ +HGain<T>(.+, probe).+" &> /dev/null
+              if [ $? -ne 0 ]; then
+
+
+                cat ../hardt/libhardt/include/$HEADER | grep 'HGain' | grep -E "^ +HGain<T>(.+, probe).+" &> /dev/null
+                if [ $? -ne 0 ]; then
+
+                  cat $IMPLEMENTATION | grep 'HIirFilter' | grep -E "^ +HIIrFilter<T>(.+, probe).+" &> /dev/null
+                  if [ $? -ne 0 ]; then
+
+
+                    cat ../hardt/libhardt/include/$HEADER | grep 'HIirFilter' | grep -E "^ +HIirFilter<T>(.+, probe).+" &> /dev/null
+                    if [ $? -ne 0 ]; then
+
+
+
+                      if [ $MISSING -eq 0 ]; then
+                        echo
+                        echo "Header: ../hardt/libhardt/include/$HEADER"
+                        echo "Classname: $CLASSNAME"
+                        echo "Implementation: $IMPLEMENTATION"
+                        echo "  Reason: No usage of probe"
+                        MISSING=1
+                      fi
+                    fi
+                  fi
+                fi
+              fi
+            fi
+          fi
+        fi
+      fi
+    done
 
   fi
 
