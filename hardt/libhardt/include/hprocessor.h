e31e012f0922d2d34b29b2d3fd3a13b8738cb4e3
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2020-11-07 09:18:05 +0100

    Update in-source documentation

--- hardt/libhardt/include/hprocessor.h
+++ hardt/libhardt/include/hprocessor.h
@@ -40,21 +40,45 @@ class HProcessor : public HWriterConsumer<T>
 
     protected:
 
-        /** The writer used by this processor */
+        /**
+         * The writer used by this processor
+         */
         HWriter<T>* _writer;
 
-        /** The reader used by this processor */
+        /**
+         * The reader used by this processor
+         */
         HReader<T>* _reader;
 
-        /** Construct a new HProcessor */
+        /**
+         * Construct a new HProcessor
+         *
+         * @param writer The writer
+         * @param reader The reader
+         * @param blocksize Number of samples to read and write per cycle
+         * @param terminationToken The termination token. Set to true to halt the processor
+         */
         HProcessor(HWriter<T>* writer, HReader<T>* reader, size_t blocksize, bool* terminationToken);
 
-        /** Construct a new HProcessor */
+        /**
+         * Construct a new HProcessor
+         *
+         * @param reader The reader
+         * @param blocksize Number of samples to read and write per cycle
+         * @param terminationToken The termination token. Set to true to halt the processor
+         */
         HProcessor(HReader<T>* reader, size_t blocksize, bool* terminationToken);
 
-        /** Default destructor */
+        /**
+         * Default destructor
+         */
         ~HProcessor();
 
+        /**
+         * Set the writer
+         *
+         * @param writer The writer to use
+         */
         void SetWriter(HWriter<T>* writer)
         {
             _writer = writer;
@@ -63,45 +87,112 @@ class HProcessor : public HWriterConsumer<T>
     public:
 
         /**
-            Read directly from the associated reader
-        */
+         * Read directly from the associated reader
+         *
+         * @param dest Destination buffer
+         * @param blocksize Number of samples to read
+         * @returns Number of blocks read
+         */
         int Read(T* dest, int blocksize);
 
         /**
-            Write directly to the associated writer
-        */
+         * Write directly to the associated writer
+         *
+         * @param src Source buffer
+         * @param blocksize Number of samples to write
+         * @returns Number of blocks written
+         */
         int Write(T* src, int blocksize);
 
         /**
-            Get the associated reader
-        */
+         * Get the associated reader
+         *
+         * @returns A HReader*
+         */
         HReader<T>* GetReader();
 
         /**
-            Get the associated writer
-        */
+         * Get the associated writer
+         *
+         * @returns A HWriter*
+         */
         HWriter<T>* GetWriter();
 
         /**
-            Read from the reader and write to the writer.
-
-            Processing stops when a zero-length read is received,
-            halt() is called or the associated terminationToken is set to true.
-        */
+         * Read from the reader and write to the writer.
+         *
+         * Processing stops when a zero-length read is received,
+         * halt() is called or the associated terminationToken is set to true.
+         *
+         * @param blocks Number of blocks to read or zero to read untill stopped (or null-read)
+         */
         virtual void Run(long unsigned int blocks = 0);
 
         /**
-            Stop processing.
-
-            If the reader and writer is not destroyed, processing can be resumed
-            by calling Run() once agan after setting the terminationToken to false
-        */
+         * Stop processing.
+         *
+         * If the reader and writer is not destroyed, processing can be resumed
+         * by calling Run() once agan after setting the terminationToken to false
+         */
         virtual void Halt();
 
-        /** Send a command to the reader and writer chain of this processor */
+        /**
+         * Send a command to the reader and writer chain of this processor
+         *
+         * @param commandClass Class of command
+         * @param commandOpcode Opcode of command
+         * @param value integer value to send as the data
+         * @returns True if the command was executed, false otherwise
+         */
+        bool Command(H_COMMAND_CLASS commandClass, H_COMMAND_OPCODE commandOpcode, int32_t value) {
+            HCommandData data;
+            data.Value = value;
+            return Command(commandClass,commandOpcode, 0, data);
+        }
+
+        /**
+         * Send a command to the reader and writer chain of this processor
+         *
+         * @param commandClass Class of command
+         * @param commandOpcode Opcode of command
+         * @param length Length of the data pointed to by 'content'
+         * @param content void* to the data  to send as the data
+         * @returns True if the command was executed, false otherwise
+         */
+        bool Command(H_COMMAND_CLASS commandClass, H_COMMAND_OPCODE commandOpcode, int16_t length, void* content) {
+            HCommandData data;
+            data.Content = content;
+            return Command(commandClass,commandOpcode, length, data);
+        }
+
+        /**
+         * Send a command to the reader and writer chain of this processor
+         *
+         * @param commandClass Class of command
+         * @param commandOpcode Opcode of command
+         * @param state Bool value to send as the data
+         * @returns True if the command was executed, false otherwise
+         */
+        bool Command(H_COMMAND_CLASS commandClass, H_COMMAND_OPCODE commandOpcode, bool state) {
+            HCommandData data;
+            data.State = state;
+            return Command(commandClass,commandOpcode, 0, data);
+        }
+
+        /**
+         * Send a command to the reader and writer chain of this processor
+         *
+         * @param commandClass Class of command
+         * @param commandOpcode Opcode of command
+         * @param length The command data length (zero for int and bool values)
+         * @param data The command data to send
+         * @returns True if the command was executed, false otherwise
+         */
         virtual bool Command(H_COMMAND_CLASS commandClass, H_COMMAND_OPCODE commandOpcode, int16_t length, HCommandData data) = 0;
 
-        /** Get the metrics object */
+        /**
+         * Get the metrics object
+         */
         HMetrics* GetMetrics() {
             return &_metrics;
         }
