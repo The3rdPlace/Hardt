380b132dee2c03147897e5403758959b78508523
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2021-02-21 17:19:38 +0100

    Add duplex mode to the hmux component
    to help turn a mono stream into a fake stereo stream for 2-channel
    output in headsets etc.

--- hardt/test/hmux_test.cpp
+++ hardt/test/hmux_test.cpp
@@ -12,6 +12,10 @@ class HMux_Test: public Test
             UNITTEST(test_mux_as_readers);
             UNITTEST(test_mux_as_writers);
             UNITTEST(test_mux_as_consumers);
+
+            UNITTEST(test_mux_as_duplex_reader);
+            UNITTEST(test_mux_as_duplex_writer);
+            UNITTEST(test_mux_as_duplex_consumer);
         }
 
         const char* name()
@@ -96,6 +100,60 @@ class HMux_Test: public Test
             ASSERT_IS_EQUAL(srcWr2.Write(stream_2, 4), 4);
             ASSERT_IS_EQUAL(wr.Writes, 2);
             ASSERT_IS_EQUAL(wr.Samples, 16);
+            ASSERT_IS_EQUAL(memcmp((void*) wr.Received, (void*) expected, 8 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_TRUE(mux.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(wr.Commands, 1);
+        }
+
+        void test_mux_as_duplex_reader()
+        {
+            int8_t stream_1[4] = {1, 2, 3, 4};
+            TestReader<int8_t> rd_1(stream_1, 4);
+            std::vector< HReader<int8_t>* > readers = { &rd_1 };
+            HMux<int8_t> mux(readers, 8, true);
+
+            int8_t input[8];
+            int8_t expected[8] = {1, 1, 2, 2, 3, 3, 4, 4};
+            DUMP(input, 8)
+            ARRAY(input, 8)
+            ASSERT_IS_EQUAL(mux.Read(input, 8), 8);
+            ASSERT_IS_EQUAL(memcmp((void*) input, (void*) expected, 8 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_TRUE(mux.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(rd_1.Commands, 1);
+        }
+
+        void test_mux_as_duplex_writer()
+        {
+            int8_t stream_1[4] = {1, 2, 3, 4};
+            TestWriter<int8_t> wr(8);
+            HMux<int8_t> mux(&wr, 1, 4, true);
+
+            int8_t expected[8] = {1, 1, 2, 2, 3, 3, 4, 4};
+            ASSERT_IS_EQUAL(mux.Write(stream_1, 4), 4);
+            ASSERT_IS_EQUAL(wr.Writes, 1);
+            ASSERT_IS_EQUAL(wr.Samples, 8);
+            ASSERT_IS_EQUAL(memcmp((void*) wr.Received, (void*) expected, 8 * sizeof(int8_t)), 0);
+
+            ASSERT_IS_TRUE(mux.Command(&TestNopCommand));
+            ASSERT_IS_EQUAL(wr.Commands, 1);
+        }
+
+        void test_mux_as_duplex_consumer()
+        {
+            int8_t stream_1[4] = {1, 2, 3, 4};
+
+            TestWriter<int8_t> srcWr1(4);
+            std::vector< HWriterConsumer<int8_t>* > consumers;
+            consumers.push_back(srcWr1.Consumer());
+            HMux<int8_t> mux(consumers, 4, true);
+            TestWriter<int8_t> wr(mux.Consumer(), 8);
+
+            int8_t expected[8] = {1, 1, 2, 2, 3, 3, 4, 4};
+            ASSERT_IS_EQUAL(srcWr1.Write(stream_1, 4), 4);
+            ASSERT_IS_EQUAL(wr.Writes, 1);
+            ASSERT_IS_EQUAL(wr.Samples, 8);
             ASSERT_IS_EQUAL(memcmp((void*) wr.Received, (void*) expected, 8 * sizeof(int8_t)), 0);
 
             ASSERT_IS_TRUE(mux.Command(&TestNopCommand));
